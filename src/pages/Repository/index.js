c4f9f0edbb60a076ccd7a03a9669f187e7d238a8
cmscoder <camila.medeiros.silva2013@gmail.com>
2025-01-16 15:39:20 +0000

    feat(repositoy): filter by status.

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Owner, Loading, BackButton } from './styles';
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+  FilterList,
+} from './styles';
 import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
@@ -7,6 +15,13 @@ export default function Repository({ match }) {
   const [repository, setRepository] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [filters, setFilters] = useState([
+    { state: 'all', label: 'All', active: true },
+    { state: 'open', label: 'Open', active: false },
+    { state: 'closed', label: 'Closed', active: false },
+  ]);
+  const [filterIndex, setFilterIndex] = useState(0);
 
   useEffect(() => {
     async function loadRepository() {
@@ -15,7 +30,7 @@ export default function Repository({ match }) {
         api.get(`/repos/${repoName}`),
         api.get(`/repos/${repoName}/issues`, {
           params: {
-            state: 'open',
+            state: filters.find((f) => f.active).state,
             per_page: 5,
           },
         }),
@@ -28,6 +43,37 @@ export default function Repository({ match }) {
 
     loadRepository();
   }, [match.params.repository]);
+
+  useEffect(() => {
+    async function loadIssues() {
+      const repoName = decodeURIComponent(match.params.repository);
+      const response = await api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: filters[filterIndex].state,
+          page,
+          per_page: 5,
+        },
+      });
+
+      setIssues(response.data);
+    }
+
+    loadIssues();
+  }, [page, filterIndex, filters, match.params.repository]);
+
+  function handlePage(action) {
+    setPage(action === 'back' ? page - 1 : page + 1);
+  }
+
+  function handleFilter(index) {
+    setFilterIndex(index);
+    const newFilters = filters.map((filter, i) => {
+      filter.active = i === index;
+      return filter;
+    });
+
+    setFilters(newFilters);
+  }
 
   if (loading) {
     return (
@@ -47,6 +93,47 @@ export default function Repository({ match }) {
         <h1>{repository.name}</h1>
         <p>{repository.description}</p>
       </Owner>
+
+      <FilterList active={filterIndex}>
+        {filters.map((filter, index) => (
+          <button
+            type="button"
+            key={filter.label}
+            onClick={() => handleFilter(index)}
+          >
+            {filter.label}
+          </button>
+        ))}
+      </FilterList>
+      <IssuesList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
+
+      <PageActions>
+        <button
+          type="button"
+          onClick={() => handlePage('back')}
+          disabled={page < 2}
+        >
+          Back
+        </button>
+        <button type="button" onClick={() => handlePage('next')}>
+          Next
+        </button>
+      </PageActions>
     </Container>
   );
 }
